bdd3849192c6a8f8d7e29b33c18540e53f83fdfa
boozhuen <76693672+boozhuen@users.noreply.github.com>
2025-04-15 13:56:08 +0800

    Update coursecontent.js

--- src/pages/coursecontent.js
+++ src/pages/coursecontent.js
@@ -260,9 +260,7 @@ const CourseContent = () => {
     <p>
       <span title="Liquidity refers to how quickly and easily you can convert something into cash without losing its value. <br> For example, cash is the most liquid asset because you can use it immediately. <br> Stocks or bonds are also relatively liquid because you can sell them quickly in the market. However, things like real estate or collectibles may take longer to sell and could be harder to convert to cash without potentially losing value. If you need the money soon, choose something easy to access." className="text-blue-600">
           Liquidity
-      </span>
-          ,in simple terms, refers to how quickly and easily you can convert something into cash without losing its value. <br>
-          If you need the money soon, choose something easy to access.
+      </span>,in simple terms, refers to how quickly and easily you can convert something into cash without losing its value. <br> If you need the money soon, choose something easy to access.
     </p>
   </div>
         
