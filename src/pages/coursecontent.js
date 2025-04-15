da07e18fa863ac3c195f4f40197590551f41f533
boozhuen <76693672+boozhuen@users.noreply.github.com>
2025-04-15 21:49:18 +0800

    Update coursecontent.js

--- src/pages/coursecontent.js
+++ src/pages/coursecontent.js
@@ -732,7 +732,7 @@ const CourseContent = () => {
               investment horizon.
             </p>
             <div style={{ marginTop: "2rem", textAlign: "center" }}>
-              <img src={Module2_1} alt="Fundamental Analysis Overview" style={{ maxWidth: "100%", height: "auto", borderRadius: "8px" }} />
+              <img src={Mod-2-1} alt="Fundamental Analysis Overview" style={{ maxWidth: "100%", height: "auto", borderRadius: "8px" }} />
             </div>
           </div>
         ),
